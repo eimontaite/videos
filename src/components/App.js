8372bc855c11e3818698afc8e7f5990f3acdb00d
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-28 14:14:26 +0300

    Add index.js, SearchBar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import youtube from '../api/youtube';
+import SearchBar from './SearchBar';
+
+class App extends React.Component { 
+    render() {
+        return (
+            <SearchBar />
+        )
+    }
+}
+
+export default App;
