f9889d36b40c80d83e59e8f729576bedc9864d28
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-28 15:22:04 +0300

    Add styling

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
 class VideoCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { spans: 0}
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10 + 1);
+
+        this.setState({ spans })
+    }
+
     render() {
         const { description, snippet } = this.props.video;
         
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img 
+                    ref={this.imageRef}
                     alt={description} 
                     src={snippet.thumbnails.default.url} />
                     <p>{snippet.title}</p>
