95b95afeed19daeb2fa9037709fa3c5d45e8ffd1
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-28 14:32:42 +0300

    Include snippet in search results

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     method: "GET",
-    baseURL: 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAhl-udsfuljI2qKsfevLpO7iAAW1CDxqE'
+    baseURL: 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAhl-udsfuljI2qKsfevLpO7iAAW1CDxqE&part=snippet'
 })
